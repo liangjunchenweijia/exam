6d6e50ad2383c552291257f042ec0c6017330095
liangjun <liangjun@grandata.cn>
2020-04-11 11:06:21 +0800

    退出

--- src/main/java/com/exam/exam_system/controller/LoginController.java
+++ src/main/java/com/exam/exam_system/controller/LoginController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 /**
  * @Author :
@@ -26,7 +27,7 @@ public class LoginController {
     }
 
     @GetMapping("/loginOut")
-    public String loginOut(HttpServletRequest request, SessionStatus sessionStatus, HttpServletResponse response) {
+    public void loginOut(HttpServletRequest request, SessionStatus sessionStatus, HttpServletResponse response) throws IOException {
         HttpSession session = request.getSession();
         session.invalidate();
         sessionStatus.setComplete();
@@ -35,6 +36,6 @@ public class LoginController {
             cookie.setMaxAge(0);
             response.addCookie(cookie);
         }
-        return "redirect:/login";
+        response.sendRedirect("/templates/login.html");
     }
 }
