802e27146b52ed9db3c1b4b46903df94c89abf9a
liangjun <liangjun@grandata.cn>
2020-04-11 10:53:27 +0800

    退出

--- src/main/java/com/exam/exam_system/controller/LoginController.java
+++ src/main/java/com/exam/exam_system/controller/LoginController.java
@@ -3,6 +3,12 @@ package com.exam.exam_system.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.support.SessionStatus;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * @Author :
@@ -11,11 +17,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @Date : 2020/3/27 15:07
  */
 @Controller
-@RequestMapping("/exam")
+@RequestMapping("/user")
 public class LoginController {
 
     @GetMapping("/login")
-    public String goLogin(){
+    public String goLogin() {
         return "login";
+    }
+
+    @GetMapping("/loginOut")
+    public String loginOut(HttpServletRequest request, SessionStatus sessionStatus, HttpServletResponse response) {
+        HttpSession session = request.getSession();
+        session.invalidate();
+        sessionStatus.setComplete();
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            cookie.setMaxAge(0);
+            response.addCookie(cookie);
+        }
+        return "redirect:/login";
     }
 }
